c551e20511511135a0a00763aed3d1b8e19efb2f
Mauricio Joel Martinez <mauricio.martinez@EDCMXENLT0470.local>
2021-05-05 11:15:05 -0500

    another issue

--- contacc/contacc/wsgi.py
+++ contacc/contacc/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contacc.contacc.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contacc.settings')
 
 application = get_wsgi_application()
+
+
+
